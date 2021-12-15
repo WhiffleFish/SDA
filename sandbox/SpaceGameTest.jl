0a7cfc7adfd171c356a6517db9463dbaf5ee7138
WhiffleFish <tylertronics@gmail.com>
2021-12-15 14:55:39 -0700

    `budget=10`, `T=20` plots

--- sandbox/SpaceGameTest.jl
+++ sandbox/SpaceGameTest.jl
@@ -1,7 +1,7 @@
 using Revise
 using CounterfactualRegret
 using SDA
-game = SDA.SpaceGame(5,20)
+game = SDA.SpaceGame(10,20)
 
 solver = ESCFRSolver(game)
 train!(solver, 10_000, show_progress=true)
@@ -10,10 +10,10 @@ using Plots
 using PyPlot
 data = solution_data(solver)
 Plots.plot(data)
-PyPlot.savefig("PolarStrategyProfile.svg")
+PyPlot.savefig("PolarStrategyProfileB10T20.svg")
 
 
-p = Plots.plot(data.σ_sat, label="Satellite Strategy", lw = 2, legend=:right)
+p = Plots.plot(data.σ_sat, label="Satellite Strategy", lw = 2, legend=:top)
 Plots.plot!(p, data.σ_ground, label="Ground Strategy", lw = 2)
 xlabel!(p, "Time Step")
 Plots.savefig(p, "LinearStrategyProfile.svg")
