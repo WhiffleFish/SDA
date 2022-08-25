1e51b7ebee907ca6c7f3ac3f5bd754dd316d06ae
WhiffleFish <tylertronics@gmail.com>
2022-08-25 13:49:10 -0600

    include earth in TrackingGame plots

--- src/TrackingGames/plotting.jl
+++ src/TrackingGames/plotting.jl
@@ -20,7 +20,7 @@ function plot_traj!(p, game, s, Δv, α=1.0) # TODO: return final state, so we d
     plot!(p, t[:,1], t[:,2], lw=3, c=:red, label="", alpha=α)
 end
 
-function plot_satellite_trajectories(sol; init_state_idx::Int=1, goal_idx::Int=1, max_depth=sol.game.max_steps-1, prob_thresh=1e-2, kwargs...)
+function plot_satellite_trajectories(sol; init_state_idx::Int=1, goal_idx::Int=1, max_depth=sol.game.max_steps-1, prob_thresh=1e-2, show_earth=true, kwargs...)
     game = sol.game
     h0 = initialhist(game)
     a_chance = chance_actions(game, h0)[init_state_idx, goal_idx]
@@ -31,6 +31,9 @@ function plot_satellite_trajectories(sol; init_state_idx::Int=1, goal_idx::Int=1
     goal_alt = I0[2]
 
     p = plot(;xticks=0, yticks=0, aspect_ratio=:equal, showaxis=false, kwargs...)
+    if show_earth
+        plot!(p, circle_shape(0,0, R_EARTH), seriestype=[:shape], lw=0.5, c=:blue, linecolor=:black, fillalpha=0.2, label="")
+    end
     plot_goal_region!(p, game, goal_alt)
     plot_sat_strats!(p, sol, I0, s0; max_depth, prob_thresh)
     return p
@@ -58,4 +61,10 @@ function plot_goal_region!(p, game, goal::AbstractFloat; kwargs...)
     y2 = @. (goal + game.tol)*sin(θ)
     plot!(p, x1, y1; c=:orange, ls=:dash, label="", kwargs...)
     plot!(p, x2, y2; c=:orange, ls=:dash, label="", kwargs...)
+end
+
+# https://discourse.julialang.org/t/plot-a-circle-with-a-given-radius-with-plots-jl/23295
+function circle_shape(h, k, r)
+    θ = 0:0.01:2π
+    h .+ r .* sin.(θ), k .+ r .* cos.(θ)
 end
