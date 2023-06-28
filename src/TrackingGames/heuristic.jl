f8665a4a8ad66334b025bb3dacb8ba7c6d05328a
WhiffleFish <tylertronics@gmail.com>
2023-06-28 18:51:46 -0400

    sat heuristic done

--- src/TrackingGames/heuristic.jl
+++ src/TrackingGames/heuristic.jl
@@ -18,6 +18,7 @@ function traverse(game::TrackingGame)
         @show goal_alt
         trav = SatHeuristicTraverser(game, goal_alt, A, game.max_steps)
         traj,cost = search(trav, s, 0)
+        @show cost
         push!(opt_traj, traj)
     end
     return opt_traj
@@ -26,10 +27,9 @@ end
 
 function search(trav::SatHeuristicTraverser, state, t)
     (;game, goal_alt, A, T) = trav
-    x,y = state
-    alt = sqrt(x^2 + y^2)
     if t == T
-        cost = 0.0
+        x,y = state
+        alt = sqrt(x^2 + y^2)
         traj = Float64[]
         cost = abs(alt - goal_alt) < game.tol ? 0.0 : Inf
         return traj, cost
@@ -40,11 +40,12 @@ function search(trav::SatHeuristicTraverser, state, t)
     for a ∈ A
         sp = step(game, state, a)
         sub_traj, cost = search(trav, sp, t+1)
+        cost += abs(a)
         if cost < best_cost
             best_cost = cost
             best_a = a
             best_traj = sub_traj
         end
     end
-    return [best_a; best_traj], abs(best_a) + best_cost
+    return [best_a; best_traj], best_cost
 end
